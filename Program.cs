80012467c54455abe3efce98b07f0e21a4eb6cd9
Adelka <s336251855@ariel.ort.org.il>
2024-10-31 13:19:26 +0200

    Created MyStack

--- Program.cs
+++ Program.cs
@@ -1,126 +1,17 @@
-﻿public class Node<T>
-{
-    private T value;
-    private Node<T>? next;
-    public Node(T value)
-    {
-        this.value = value;
-        this.next = null;
-    }
-
-    public Node(T value, Node<T> next)
-    {
-        this.value = value;
-        this.next = next;
-    }
-
-    public T GetValue()
-    {
-        return value;
-    }
-
-    public Node<T>? GetNext()
-    {
-        return next;
-    }
-
-    public void SetValue(T value)
-    {
-        this.value = value;
-    }
-
-    public void SetNext(Node<T> next)
-    {
-        this.next = next;
-    }
-
-    public bool HasNext()
-    {
-        return next != null;
-    }
-
-    public override string ToString()
-    {
-        return value!.ToString()!;
-    }
-}
-public static class ListOperations
-{
-    public static void AddToTail<T>(Node<T> head, T info)
-    {
-        while (head.HasNext())
-        {
-            head = head.GetNext()!;
-        }
-        head.SetNext(new Node<T>(info));
-    }
-
-    public static string ToString<T>(Node<T> head)
-    {
-        string result = "";
-        while (head != null)
-        {
-            result += head.ToString() + ",";
-            head = head.GetNext();
-        }
-        return result;
-
-    }
-    public static int GetLength<T>(Node<T> head)
-    {
-        int len = 0;
-
-        while (head != null)
-        {
-            len++;
-            head = head.GetNext()!;
-        }
-        return len;
-    }
-
-    public static bool Contains<T>(Node<T> head, T value)
-    {
-        while (head != null)
-        {
-            if (head.GetValue().Equals(value))
-            {
-                return true;
-            }
-            head = head.GetNext()!;
-        }
-        return false;
-    }
-
-    public static int GetMax<T>(Node<int> head)
-    {
-        int max = int.MinValue;
-        while (head != null)
-        {
-            if (head.GetValue() > max)
-            {
-                max = head.GetValue();
-            }
-            head = head.GetNext()!;
-        }
-        return max;
-    }
-
-    public static void Insert<T>(Node<T> head, T value, int index)
-    {
-        for (; index > 1; index--)
-        {
-            head = head.GetNext()!;
-        }
-        Node<T> temp = new(value);
-        temp.SetNext(head.GetNext());
-        head.SetNext(temp);
-    }
-}
+﻿namespace AniHamelech;
 class Program
 {
     public static void Main()
     {
-        Show(Build(5, 20, 5));
+        MyStack<int> Stack = new();
+
+        Stack.Push(20);
+        Stack.Push(40);
+        Stack.Push(60);
+
+        Stack.Pop();
+        Stack.Pop();
+        Console.WriteLine(Stack.Contains(0));
     }
     public static Node<int> Build()
     {
---
+++ Stack/MyStack.cs
@@ -0,0 +1,53 @@
+using System.Runtime.Intrinsics.X86;
+
+namespace AniHamelech;
+public class MyStack<T>
+{
+    public T[] items;
+    public MyStack()
+    {
+        this.items = [];
+    }
+    public T Pop()
+    {
+        T temp = items[0];
+        items = items.Skip(1).ToArray();
+        return temp;
+    }
+    public void Push(T value)
+    {
+        items = [value, .. items];
+    }
+    public T Peek()
+    {
+        return items[0];
+    }
+    public bool IsEmpty()
+    {
+        return items[0] == null;
+    }
+
+    public void SwapLast2()
+    {
+        T v1 = items[0];
+        T v2 = items[1];
+
+        items = items.Skip(2).ToArray();
+        items = [v2, v1, .. items];
+    }
+
+    public bool Contains(T value)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i]!.Equals(value)) { return true; }
+        }
+        return false;
+    }
+    public bool IsFull()
+    {
+        return items.Length == int.MaxValue;
+    }
+
+
+}
